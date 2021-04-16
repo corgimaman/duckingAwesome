63f0b57f76decdca0642de4c662587b85eadf318
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-04-15 19:04:46 -0500

    removed more console.logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,9 +29,7 @@ function handleMessage(jsonTransfer) {
      for (var i = removeValFromIndex.length - 1; i >= 0; i--)
     jsonTransfer.splice(removeValFromIndex[i], 1);
     // shuffle the json
-    jsonTransfer.sort(() => Math.random() - .5);
-    //console.log(jsonTransfer)
-    
+    jsonTransfer.sort(() => Math.random() - .5);    
 
     // taking the entire array and replacing the api queries
      for (let i = 0; i < jsonTransfer.length; i++) {
@@ -50,15 +48,11 @@ function handleMessage(jsonTransfer) {
 
      //pull the first 20 URLs for the API call
      apiArray = foArray.slice(0, 20);
-    //  console.log('Api aray $$$$',apiArray);
 
      //pull the first 20 names for the button
      btnArray = btnArray.slice(0, 20)
-    //  console.log('Button ArRAY $$$$',btnArray);
     
     hiddenSpan();
-
-     //console.log(spanArray);
 
 }
 
@@ -69,7 +63,6 @@ function hiddenSpan(){
     let counter = 0
     // for loop runs through each api call and adds each response to spanArray 
     for (let i = 0; i < apiArray.length; i++) {
-        //if apiArray.length === 0 return, else run function
         fetch(apiArray[i], { headers: {  'Content-Type': 'application/json', 'Accept': 'text/plain' } })
         .then(response => response.text())
         .then(text => {
@@ -81,7 +74,6 @@ function hiddenSpan(){
                 makeBtn()
             }
         })
-        //.then(makeBtn())
     } 
     
 }
@@ -94,7 +86,7 @@ function makeBtn(){
     for (let i = 0; i < spanArray.length; i++) {
 
         var btn = document.createElement("button");
-        btn.innerHTML = censor(btnArray[i])// + '<span class="speakSpan" style="display: none;">' + spanArray[i] + '</span>';
+        btn.innerHTML = censor(btnArray[i]);
         btn.classList.add("button", "is-large", "is-info", "is-outlined", "speakClass");
         btn.setAttribute('name', censor(spanArray[i]));
         btn.setAttribute('id', 'btn' + [i]);
